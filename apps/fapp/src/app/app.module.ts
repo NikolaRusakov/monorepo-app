108660206d1a22eaa89dc6d07e308a79ea9745df
Nikola Rusakov <nikola.rusakov@rusakov.cz>
2019-12-06 22:34:25 +0100

    feat(fapp): add ionic and pwa functionality

--- apps/fapp/src/app/app.module.ts
+++ apps/fapp/src/app/app.module.ts
@@ -2,10 +2,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ServiceWorkerModule } from '@angular/service-worker';
+import { environment } from '../environments/environment';
+import { IonicModule } from '@ionic/angular';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [BrowserModule, ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }), IonicModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
